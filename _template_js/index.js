398a729b8acdee174830c6502757369ebc6cd106
deskoh <deskoh.sg@gmail.com>
2021-03-14 16:42:22 +0800

    Buffer writes to stdout

--- _template_js/index.js
+++ _template_js/index.js
@@ -7,6 +7,10 @@ const rl = readline.createInterface({
   input: process.stdin,
 });
 
+// Buffer output until `end()` is called
+// See https://nodejs.org/api/stream.html#stream_writable_cork
+process.stdout.cork();
+
 rl.on('line', (line) => {
   process.stdout.write(`${line}\n`);
 });
--- _template_ts/index.ts
+++ _template_ts/index.ts
@@ -6,6 +6,10 @@ const rl = readline.createInterface({
     input: process.stdin,
 });
 
+// Buffer output until `end()` is called
+// See https://nodejs.org/api/stream.html#stream_writable_cork
+process.stdout.cork();
+
 rl.on('line', (line) => {
   process.stdout.write(`${line}\n`);
 });
